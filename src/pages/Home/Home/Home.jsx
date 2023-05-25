31df7989401c516fad6597bad2235da4a405b84b
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-25 12:16:06 +0600

    Create simple Top Banner react responsive

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <Banner></Banner>
         </div>
     );
 };
