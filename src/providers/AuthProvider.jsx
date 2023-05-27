e477103ed8b3c6c808870f975abfa906f7bdcb94
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-27 15:28:31 +0600

    Firebase project setup for bistro boss

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+// import { getAuth } from "firebase/auth";
+// import { app } from "../firebase/firebase.config";
+
+
+export const AuthContext = createContext(null);
+// const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const authInfo={
+        user,
+        loading
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
