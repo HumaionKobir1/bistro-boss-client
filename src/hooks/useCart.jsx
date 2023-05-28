d3edb44ea9a3723417b2cd0e01233f2070585490
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-28 12:07:22 +0600

    Simple overview of react query and load data

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,10 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+
 
 const useCart = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user} = useContext(AuthContext);
+
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`)
+            return response.json();
+        } ,
+      })
+    
+    return [cart, refetch]
 };
 
 export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Router.jsx'
 import AuthProvider from './providers/AuthProvider'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-          <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>    
+      </QueryClientProvider>       
     </AuthProvider>
   </React.StrictMode>,
 )
