5b232804ad408cff1c8d2ca2a67eab07cab6e3c9
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-29 17:19:30 +0600

    Send JWT token to back end end for verification

--- src/pages/DashBoard/AllUser.jsx
+++ src/pages/DashBoard/AllUser.jsx
@@ -29,7 +29,7 @@ const AllUser = () => {
     }
 
     const handleDelete = user => {
-
+        console.log(user);
     }
     return (
         <div className="w-full">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 const provider = new GoogleAuthProvider();
 
 
@@ -36,6 +37,19 @@ const AuthProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe =  onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data => {
+                    console.log(data.data.token);
+                    localStorage.setItem('access-token', data.data.token)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
             setLoading(false);
         });
         return () => {
