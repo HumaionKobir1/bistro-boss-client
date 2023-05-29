94abb9632253a6663e3ec63d45d555c2d8805e41
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-29 15:19:38 +0600

    Display all users and create make admin API

--- src/pages/DashBoard/AllUser.jsx
+++ src/pages/DashBoard/AllUser.jsx
@@ -1,13 +1,52 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 
 const AllUser = () => {
     const {data: users = [], refetch} = useQuery(['users'], async() => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
+
+    const handleDelete = user => {
+
+    }
     return (
-        <div>
-            {users.length}
+        <div className="w-full">
+            <Helmet>
+                <title>Bistro Boss | All users</title>
+            </Helmet>
+            <h3 className="text-2xl font-semibold my-4">Total Users: {users.length}</h3>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        users.map((user, index) => <tr
+                            key={user._id}
+                        >
+                            <th>{index + 1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.role === 'admin' ? 'admin' : <button className="btn btn-ghost btn-sm bg-orange-400"><FaUserShield></FaUserShield></button> }</td>
+                            <td><button onClick={()=>handleDelete(user)} className="btn btn-ghost btn-sm bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+</td>
+                        </tr>)
+                    }
+                    
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
