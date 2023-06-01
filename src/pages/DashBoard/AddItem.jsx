ad0d543b098d561b4ae0f0a39e650949fbfda358
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-01 13:20:54 +0600

    Creata add item form

--- src/pages/DashBoard/AddItem.jsx
+++ src/pages/DashBoard/AddItem.jsx
@@ -1,8 +1,92 @@
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 const AddItem = () => {
+
+
+    const handleAddItem = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const category = form.category.value;
+        const details = form.details.value;
+
+        const newToy = {category, name, price,  details};
+        console.log(newToy);
+    }
     return (
-        <div>
-            <h1>this is add item page</h1>
+        <div className="w-full">
+            
+            <SectionTitle
+                subHeading={"What's New"}
+                heading={"Add an Item"}
+            ></SectionTitle>
+
+
+            <div className="w-full md:w-5/6 mx-auto px-3 py-5 bg-gray-100">
+                <h1 className="text-2xl font-bold my-4 text-center text-gray-600">Add A Toy</h1>
+                <form onSubmit={handleAddItem} className="md:w-9/12 mx-auto rounded-lg bg-white shadow-lg p-8">
+                    <div className="mb-4">
+                        
+                        <div className="w-full md:mb-0 mb-4">
+                        <label htmlFor="Title" className="block font-medium">Name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="border border-gray-300 p-2 w-full"
+                            required
+                        />
+                        </div>
+                    </div>
+
+                    
+
+                    <div className="mb-4 md:flex gap-5">
+                        <div className="w-full md:mb-0 mb-4">
+                        <label htmlFor="category" className="block font-medium">Category:</label>
+                        <select
+                            id="category"
+                            className="border border-gray-300 p-2 w-full"
+                            name="category"
+                            required
+                        >
+                            <option disabled selected>Pick one</option>
+                            <option value="pizza">Pizza</option>
+                            <option value="soup">Soup</option>
+                            <option value="salad">Salad</option>
+                            <option value="dessert">Dessert</option>
+                            <option value="drinks">Drinks</option>
+                        </select>
+                        </div>
+                        <div className="w-full md:mb-0 mb-4">
+                        <label htmlFor="price" className="block font-medium">Price:</label>
+                        <input
+                            type="number"
+                            id="price"
+                            name="price"
+                            className="border border-gray-300 p-2 w-full"
+                            required
+                        />
+                        </div>
+                    </div>
+                    
+                    
+                    <div className=" md:mb-0 mb-4">
+                    <label htmlFor="description" className="block font-medium">Detail Description:</label>
+                    <textarea
+                        id="description"
+                        className="border border-gray-300 p-2 w-full h-24"
+                        name="details"
+                        required
+                    ></textarea>
+                    </div>
+                    <button type="submit" className="w-full bg-[#D1A054] hover:bg-[rgb(231,161,144)] text-gray-800 font-bold py-2 px-4 rounded">
+                    Add Item
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
