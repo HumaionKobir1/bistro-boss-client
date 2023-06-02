4e62e67fead73a98fdcdcbd2285069e084d707e8
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-02 12:39:56 +0600

    install stripe, stripe for react and create card element

---
+++ src/pages/DashBoard/CheckoutForm.jsx
@@ -0,0 +1,43 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements){
+            return;
+        }
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                style: {
+                    base: {
+                    fontSize: '16px',
+                    color: '#424770',
+                    '::placeholder': {
+                        color: '#aab7c4',
+                    },
+                    },
+                    invalid: {
+                    color: '#9e2146',
+                    },
+                },
+                }}
+            />
+            <button type="submit" disabled={!stripe}>
+                Pay
+            </button>
+            </form>
+        </div>
+    );
+};
+
+export default CheckoutForm;
