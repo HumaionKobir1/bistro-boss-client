9141120115a9e3d31059e85fd3e1a316ca25725f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-03 14:30:27 +0600

    Create payment api and send success message

--- src/pages/DashBoard/Payment.jsx
+++ src/pages/DashBoard/Payment.jsx
@@ -9,7 +9,7 @@ const Payment = () => {
     const total = cart.reduce((sum, item) => sum + item.price, 0);
     const price = parseFloat(total.toFixed(2))
 
-    const stripePromise = loadStripe('pk_test_51NETPuB9B9Ycv5F9H1vLDTjnKJ9Emytr1V98rYnGg7nlqLHt6QySQli0UEUJaxb3uINN40E9JS9fpL71cTMlX6aR00d6K6182B')
+    const stripePromise = loadStripe(process.meta.env.VITE_Payment_Gateway_pk)
     return (
         <div className="w-full md:p-20 p-5">
             <SectionTitle
